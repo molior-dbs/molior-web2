48baf9d43d786debf7a6174b9f116d64f9fbaf7c
Karol Swiderski <szakalboss@gmail.com>
2020-10-13 08:28:20 +0000

    projectinfo: fix builds table
    Signed-off-by: Karol Swiderski <szakalboss@gmail.com>

--- src/app/components/projectversion/projectversion-build-list.ts
+++ src/app/components/projectversion/projectversion-build-list.ts
@@ -1,4 +1,4 @@
-import {Component, ElementRef, ViewChild, Input, OnInit} from '@angular/core';
+import {Component, ElementRef, ViewChild, Input} from '@angular/core';
 import {ActivatedRoute, ParamMap} from '@angular/router';
 
 import {ProjectVersion, ProjectVersionService} from '../../services/project.service';
@@ -9,25 +9,21 @@ import {ProjectVersion, ProjectVersionService} from '../../services/project.serv
     templateUrl: './projectversion-build-list.html',
     styleUrls: ['./projectversion-build-list.scss']
 })
-export class ProjectversionBuildListComponent implements OnInit {
+export class ProjectversionBuildListComponent {
     buildicon;
     projectversion: ProjectVersion;
 
     constructor(protected route: ActivatedRoute,
                 protected projectversionService: ProjectVersionService) {
-        this.projectversion = {id: -1, name: '', is_locked: false,
-                               project_name: '',
-                               apt_url: '', architectures: [], basemirror: '', is_mirror: false, description: '',
-                               dependency_policy: 'strict', ci_builds_enabled: false};
-    }
-
-    ngOnInit() {
         this.route.parent.paramMap.subscribe((params: ParamMap) => {
             const version = params.get('version');
             this.route.parent.parent.paramMap.subscribe((params2: ParamMap) => {
                 const name = params2.get('name');
-                this.projectversionService.get(name, version).subscribe((res: ProjectVersion) => this.projectversion = res);
+                this.projectversionService.get(name, version).subscribe((res: ProjectVersion) => {
+                    this.projectversion = res;
+                });
             });
         });
     }
+
 }
