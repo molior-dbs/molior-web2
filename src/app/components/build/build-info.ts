e4e56390d38ab847c80026e6c18379d1f74640b9
André Roth <neolynx@gmail.com>
2021-01-03 13:53:06 +0000

    error finder: only search errors on failed builds

--- src/app/components/build/build-info.ts
+++ src/app/components/build/build-info.ts
@@ -199,62 +199,65 @@ export class BuildInfoComponent implements OnInit, OnDestroy, AfterViewInit {
                     logline.innerHTML = this.ansiup.ansi_to_html(line);
                     logline.className = 'logline';
 
-                    // head: cannot open '/etc/ssl/certs/java/cacerts' for reading: No such file or directory
-                    const patterns = [
-                        // this       but not any of all of that
-                        [/\S*(error|ERROR|Error): /i, [
-                            [/dpkg-buildpackage: error: debian\/rules build subprocess returned exit status \d+$/],
-                            [/sbuild command failed/],
-                            [/dpkg-buildpackage/, /exit status \d+/],
-                        ]],
-                        [/^[^/(]*[^;]\berror\b[^:]/i, [
-                            [/gpgv: keyblock resource/, /General error$/],
-                            [/error\.\S+$/],
-                            [/dpkg-buildpackage/, /exit status \d+/],
-                            [/: warning: unused parameter/],
-                        ]],
-                        [/^(\x1b[^m]+m)*E:/, [
-                            [/dpkg-buildpackage died/],
-                            [/Error building source package/],
-                            [/Package build dependencies not satisfied; skipping/],
-                        ]],
-                        [/^(\x1b[^m]+m)*make.+No rule to make target.*Stop/, []],
-                        [/dh_install: missing files, aborting/, []],
-                        [/\/bin\/sh:.+not found/, []],
-                        [/: No such file or directory/, [
-                            [/head: cannot open/, /certs\/java\/cacerts/],
-                            [/aclocal: warning: couldn't open directory 'm4'/],
-                        ]],
-                        [/Target "[^"]" does not exist in the project/, []],
-                        [/\.py:\d+:\d+: [FW]\d+ /, []],
-                        [/dh_systemd_enable: Could not handle all of the requested services/, []],
-                        [/unsat-dependency: /, []],
-                        [/: Permission denied/, []],
-                        [/: error :/, []],
-                        [/: error CS\d+:/, []],
-                    ];
-                    for (const pattern of patterns) {
-                        if (line.search(pattern[0] as RegExp) >= 0) {
-                            const falsepositives = pattern[1] as [];
-                            let found = false;
-                            for (const fps of falsepositives) {
-                                found = true;
-                                for (const fp of fps as RegExp[]) {
-                                    if (line.search(fp) === -1) {
-                                        found = false;
+                    // Error Finder
+                    if (this.build.buildstate !== 'successful') {
+                        const patterns = [
+                            // this                       but not any of all of that
+                            [/\S*(error|ERROR|Error): /i, [
+                                [/dpkg-buildpackage: error: debian\/rules build subprocess returned exit status \d+$/],
+                                [/sbuild command failed/],
+                                [/dpkg-buildpackage/, /exit status \d+/],
+                            ]],
+                            [/^[^/(]*[^;]\berror\b[^:]/i, [
+                                [/gpgv: keyblock resource/, /General error$/],
+                                [/error\.\S+$/],
+                                [/dpkg-buildpackage/, /exit status \d+/],
+                                [/: warning: unused parameter/],
+                            ]],
+                            [/^(\x1b[^m]+m)*E:/, [
+                                [/dpkg-buildpackage died/],
+                                [/Error building source package/],
+                                [/Package build dependencies not satisfied; skipping/],
+                            ]],
+                            [/^(\x1b[^m]+m)*make.+No rule to make target.*Stop/, []],
+                            [/dh_install: missing files, aborting/, []],
+                            [/\/bin\/sh:.+not found/, []],
+                            [/: No such file or directory/, [
+                                [/head: cannot open/, /certs\/java\/cacerts/],
+                                [/aclocal: warning: couldn't open directory 'm4'/],
+                            ]],
+                            [/Target "[^"]" does not exist in the project/, []],
+                            [/\.py:\d+:\d+: [FW]\d+ /, []],
+                            [/dh_systemd_enable: Could not handle all of the requested services/, []],
+                            [/unsat-dependency: /, []],
+                            [/: Permission denied/, []],
+                            [/: error :/, []],
+                            [/: error CS\d+:/, []],
+                        ];
+                        for (const pattern of patterns) {
+                            if (line.search(pattern[0] as RegExp) >= 0) {
+                                const falsepositives = pattern[1] as [];
+                                let found = false;
+                                for (const fps of falsepositives) {
+                                    found = true;
+                                    for (const fp of fps as RegExp[]) {
+                                        if (line.search(fp) === -1) {
+                                            found = false;
+                                            break;
+                                        }
+                                    }
+                                    if (found) {
                                         break;
                                     }
                                 }
-                                if (found) {
+                                if (!found) {
+                                    row.className = 'errorline';
                                     break;
                                 }
                             }
-                            if (!found) {
-                                row.className = 'errorline';
-                                break;
-                            }
                         }
                     }
+
                     this.loglines += 1;
                     lastrow = row;
                 });
