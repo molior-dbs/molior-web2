4c0f12780c0ef2060c28a5ce6532e788e848378b
André Roth <neolynx@gmail.com>
2021-01-08 20:10:54 +0000

    about: add gpg key and link to api doc

--- src/app/components/about/about.ts
+++ src/app/components/about/about.ts
@@ -5,6 +5,10 @@ import {apiURL} from '../../lib/url';
 
 interface Status {
     sshkey: string;
+    gpgurl: string;
+    version: string;
+    maintenance_message: string;
+    maintenance_mode: boolean;
 }
 
 @Component({
@@ -14,14 +18,18 @@ interface Status {
 })
 export class AboutComponent implements OnInit {
     status: Status;
+    repoasc: string;
 
     constructor(protected http: HttpClient) {
-        this.status = {sshkey: ''};
+        this.status = {sshkey: '', gpgurl: '', version: '', maintenance_message: '', maintenance_mode: false};
+        this.repoasc = '';
     }
 
     ngOnInit() {
-        this.http.get<Status>(`${apiURL()}/api/status`).subscribe(
-            r => this.status = r,
+        this.http.get<Status>(`${apiURL()}/api/status`).subscribe(r => {
+            this.status = r;
+            this.http.get<string>(this.status.gpgurl, {responseType: 'text' as 'json'}).subscribe(r2 => this.repoasc = r2);
+            },
             err => console.log('Error getting server status', err)
         );
     }
