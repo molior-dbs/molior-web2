3b0b17117bf688bda21b0e8262e24a3e6056e4d4
André Roth <neolynx@gmail.com>
2021-02-12 22:18:32 +0000

    validation: fix git urls

--- src/app/services/validation.service.ts
+++ src/app/services/validation.service.ts
@@ -4,7 +4,7 @@ import {FormGroup, FormControl, AbstractControl} from '@angular/forms';
 export const urlregex = /^(?:https?:\/\/)(?:([\w\._-]+)(\.[\w\._-]+)+(:\d+)?)(?:(\/[\w\._-]+))+$/;
 export const gitregex = [
     /^(https?:\/\/)(?:([\w\._-]+)(\.[\w\._-]+)+(:\d+)?)(?:(\/[\w\._-]+))+$/,
-    /^(ssh:\/\/)?(git@)(?:([\w\._-]+)(\.[\w\._-]+)+(:\d+)?):[\w\._-]+(\/[\w\._-]+)+$/,
+    /^(ssh:\/\/)?(\w+@)?(?:([\w\._-]+)(\.[\w\._-]+)+(:\d+)?)[:/][\w\._-]+(\/[\w\._-]+)+$/,
 ];
 
 export class ValidationService {
